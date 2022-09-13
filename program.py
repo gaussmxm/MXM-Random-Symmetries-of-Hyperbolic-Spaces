df910cdaf1cf093e955748c24ed33e40b3e02887
gaussmxm <112975290+gaussmxm@users.noreply.github.com>
2022-09-13 21:12:46 +0000

    Rework program file

--- program.py
+++ program.py
@@ -1,19 +1,9 @@
-# Implementation of the algorithm in "An Algorithm for 2-Generator Fuchsian
-# Groups" by J. Gilman and B. Maskit (doi:10.1307/mmj/1029004258).
-#
-# The program takes as input two matrices in GL(2,Z) that have positive
-# determinants and determines whether the subgroup of PSL(2,R) that they
-# generate is non-elementary, free, and discrete.
+# Partial implementation of the algorithm in "An Algorithm for 2-Generator
+# Fuchsian Groups" by J. Gilman and B. Maskit (doi:10.1307/mmj/1029004258).
 
-# NOTE: Once we hit an elliptic element, we are done; the subgroup is either
-# not free or not discrete (see the middle of page 14 in the paper).
-
-# Matrices are represented by lists with 4 elements. For example, the matrix
-#
-# [ a b ]
-# [ c d ]
-#
-# is encoded as the list [a, b, c, d].
+# The algorithm takes as input two entries of SL(2,Z) and decides whether or
+# not the subgroup of PSL(2,Z) that they generate is non-elementary, free, and
+# discrete.
 
 #### Imports ####
 
@@ -22,82 +12,70 @@ import sys
 
 #### Common Methods ####
 
-# Computes the trace of the matrix m.
-
-def trace(m):
-    return m[0] + m[3]
-
-# Computes the determinant of the matrix m.
-
 def determinant(m):
     return (m[0] * m[3]) - (m[1] * m[2])
-
-# Computes the product m1 * m2 of the matrices m1 and m2.
-
-def matrixMultiply(m1, m2):
-    return [(m1[0] * m2[0]) + (m1[1] * m2[2]),
-            (m1[0] * m2[1]) + (m1[1] * m2[3]),
-            (m1[2] * m2[0]) + (m1[3] * m2[2]),
-            (m1[2] * m2[1]) + (m1[3] * m2[3])]
-
-# Compute the quantity used throughout the algorithm for the matrix m. The
-# quantity is given by T(m) = tr(m) / sqrt(det(m)). Note that this quantity is
-# always well-defined, since the matrices we work with always have positive
-# determinants.
-
-def T(m):
-    return trace(m) / math.sqrt(determinant(m))
-
-# Computes the matrix given by negating all entries of the input matrix.
 
 def negate(m):
     return [-m[0], -m[1], -m[2], -m[3]]
 
+def trace(m):
+    return m[0] + m[3]
+
+def T(m):
+    return trace(m) / math.sqrt(determinant(m))
+
 #### Argument Parsing and Sanity Checks ####
 
-# NOTE: This method of reading in the matrices is quite ugly. Don't @ me.
-#
-# For a pair of matrices g = [a, b, c, d] and h = [w, x, y, z], invoke the
-# program as
-#
-# python3 program.py a b c d w x y z
-#
-# to run the algorithm on g and h.
+# NOTE: Inputting the matrices is not implemented yet. For now just edit the
+# values for g and h to change them.
 
-if len(sys.argv) != 9:
-    print("Error: Expected 8 arguments (2 matrices, 4 entries each).")
+g = [1, 0, 0, 1]
+h = [1, 0, 0, 1]
+
+if determinant(g) != 1:
+    print("Error: The matrix g is not in SL(2,Z).")
     sys.exit(1)
 
-for i in range(1, 9):
-    try:
-        sys.argv[i] = int(sys.argv[i])
-    except:
-        print("Error: Could not parse an argument into an integer.")
-        sys.exit(1)
-
-g = sys.argv[1:5]
-h = sys.argv[5:]
-
-# NOTE: End of ugliness.
-
-if determinant(g) <= 0:
-    print("Error: The determinant of g is not positive.")
+if determinant(h) != 1:
+    print("Error: The matrix h is not in SL(2,Z).")
     sys.exit(1)
 
-if determinant(h) <= 0:
-    print("Error: The determinant of h is not positive.")
+#### Algorithm Cases ####
+
+def caseOne(g, h):
+    print("Error: Case I is not implemented yet.")
     sys.exit(1)
 
-#### 0. First Computations ####
+def caseTwo(g, h):
+    print("Error: Case II is not implemented yet.")
+    sys.exit(1)
 
-#### 1. Hyperbolic-Hyperbolic ####
+def caseThree(g, h):
+    print("Error: Case III is not implemented yet.")
+    sys.exit(1)
 
-#### 2. Hyperbolic-Parabolic ####
+#### Algorithm Start ####
 
-#### 3. Parabolic-Parabolic ####
+print("Inputted matrices:")
+print()
+print(f"g = {g}")
+print(f"h = {h}")
+print()
 
-#### 4. Elliptic-Hyperbolic ####
+if T(g) < 0:
+    g = negate(g)
 
-#### 5. Elliptic-Parabolic ####
+if T(h) < 0:
+    h = negate(h)
 
-#### 6. Elliptic-Elliptic ####
+if T(g) > T(h):
+    g, h = h, g
+
+if T(g) > 2:
+    caseOne(g, h)
+elif T(g) == 2 and T(h) > 2:
+    caseTwo(g, h)
+elif T(g) == 2 and T(h) == 2:
+    caseThree(g, h)
+else:
+    print("Result: The subgroup < g, h > is either not free or not discrete.")
